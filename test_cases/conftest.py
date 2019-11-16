e6660a75719080d52eb82d818ab1bfae7bc5ab86
han-zhichao <2375247815@qq.com>
2019-11-16 16:39:56 +0800

    修改为固定的报告名

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -3,10 +3,10 @@ import os
 from utils.notify import Notice
 
 
-def pytest_configure(config):
-    now = datetime.now().strftime("%Y%m%d_%H%M%S")
-    config.option.htmlpath = os.path.join(
-        config.rootdir, 'reports', f'report_{now}.html')
+# def pytest_configure(config):
+#     now = datetime.now().strftime("%Y%m%d_%H%M%S")
+#     config.option.htmlpath = os.path.join(
+#         config.rootdir, 'reports', f'report_{now}.html')
 
     # config.option.log_file = os.path.join(
     #     config.rootdir, 'reports', f'pytest_{now}.log')
